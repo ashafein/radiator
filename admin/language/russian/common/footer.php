f7e01e14c5cde3b9b539c36144036a5783a8715c
Julia <julia@julia-K53SM.(none)>
2013-02-28 19:20:11 +0200

    Revert "opencart systemwith needed modules"
    This reverts commit 6961dc874eee9e4e8c1ecaf37b291617bb80694b.

--- admin/language/russian/common/footer.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Text
-$_['text_footer'] = 'Данный релиз подготовлен командой <a href="http://www.opencart.ru">opencart.ru</a>';
-?>
--- admin/view/template/common/footer.tpl
+++
@@ -1,3 +0,0 @@
-</div>
-<div id="footer"><img src="http://www.opencart.ru/spacer.gif"><br /><?php echo $text_footer; ?></div>
-</body></html>
