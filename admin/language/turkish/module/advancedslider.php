5b4dbe5acaa5840e8f9a33aaca32fcff67d18a71
Julia <julia@julia-K53SM.(none)>
2013-02-28 19:20:43 +0200

    Revert "Revert "opencart systemwith needed modules""
    This reverts commit f7e01e14c5cde3b9b539c36144036a5783a8715c.

---
+++ admin/language/turkish/module/advancedslider.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']       = 'Gelişmiş Slayt 2.0 Beta 2';
+
+// Text
+$_['text_module']         = 'Modüller';
+$_['text_success']        = 'Başarılı: Gelişmiş Slayt modülü güncellendi!';
+$_['text_content_top']    = 'Üst Tarafta';
+$_['text_content_bottom'] = 'Alt Tarafta';
+$_['text_column_left']    = 'Sol Blokta';
+$_['text_column_right']   = 'Sağ Blokta';
+$_['text_type']   = 'Tip_';
+
+// Entry
+$_['entry_slider']        = 'Slayt:';
+$_['entry_dimension']     = 'Boyut (G x Y):';
+$_['entry_layout']        = 'Bölüm:';
+$_['entry_velocity']      = 'Geçiş Hızı:';
+$_['entry_duration']      = 'Gösterim Süresi:';
+$_['entry_type']      	  = 'Slayt Tipi:';
+$_['entry_position']      = 'Pozisyon:';
+$_['entry_status']        = 'Durumu:';
+$_['entry_sort_order']    = 'Sıralama:';
+
+// Error
+$_['error_permission']    = 'Uyarı: Gelişmiş Slayt modülünü değiştirme iznine sahip değilsiniz!';
+$_['error_dimension']     = 'Genişlik &amp; Yükseklik boyutları gerekli!';
+?>
---
+++ catalog/model/fido/homepage.php
@@ -0,0 +1,43 @@
+<?php
+class ModelFidoHomepage extends Model {
+	public function getHomepages() {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "homepage h LEFT JOIN " . DB_PREFIX . "homepage_description hd ON (h.homepage_id = hd.homepage_id) LEFT JOIN " . DB_PREFIX . "homepage_to_store h2s on (h.homepage_id = h2s.homepage_id) WHERE h.status = '1' AND hd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND h2s.store_id = '" . (int)$this->config->get('config_store_id') . "' ORDER BY h.sort_order ASC");
+		return $query->rows;
+	}
+
+	function getHomepageModules() {
+		$this->language->load('module/homepage');
+		$exempted_modules = file(DIR_SYSTEM . 'helper/exempted_modules.txt');
+		$exempted_list = explode(',', $exempted_modules[0]);
+		$module_data = array();
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "extension WHERE `type` = 'module'");
+		foreach ($query->rows as $result) {
+			if ($this->config->get($result['key'] . '_status') && ($this->config->get($result['key'] . '_position') == 'home')) {
+				if (in_array($result['key'], $exempted_list)) {
+					$exempted = TRUE;
+					$welcomed = $this->config->get($result['key'] . '_welcomed');
+					$tabbed = FALSE;
+				} else {
+					$exempted = FALSE;
+					$welcomed = FALSE;
+					$tabbed = $this->config->get($result['key'] . '_tabbed');
+				}
+				$this->load->language('module/' . $result['key']);
+				$module_data[] = array(
+					'code'       => $result['key'],
+					'exempted'   => $exempted,
+					'welcomed'   => $welcomed,
+					'tabbed'     => $tabbed,
+					'sort_order' => $this->config->get($result['key'] . '_sort_order')
+				);
+			}
+		}
+		$sort_order = array(); 
+		foreach ($module_data as $key => $value) {
+      		$sort_order[$key] = $value['sort_order'];
+    	}
+    	array_multisort($sort_order, SORT_ASC, $module_data);
+    	return $module_data;
+	}
+}
+?>
