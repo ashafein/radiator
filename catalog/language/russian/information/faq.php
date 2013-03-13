0e8cb4551899df6cee3d36a43aed4774fff052b0
Julia <julia@julia-K53SM.(none)>
2013-03-13 15:57:25 +0200

    files added

---
+++ catalog/language/russian/information/faq.php
@@ -0,0 +1,12 @@
+<?php
+// Heading 
+$_['heading_title']    = 'Часто задаваемые вопросы';
+
+// Text
+$_['text_faq']	       = 'FAQs';
+$_['text_error']	   = 'FAQ Страница не найдена!';
+$_['text_description'] = '<p>Получите ответы на часто задаваемые вопросы!</p>';
+
+// Buttons
+$_['button_faq']       = 'FAQs';
+?>
